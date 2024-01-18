e01f213c5d0ce28621d1d75773eda41c9dcb3d34
aref <arefshal@yahoo.com>
2024-01-18 11:57:29 +0330

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Aref on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
